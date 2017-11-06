4a49a1180fdb83bc5b99299bc71296cd2647148c
Yves Schelpe <yves.schelpe@gmail.com>
2017-11-06 20:00:33 +0100

    Added initial startup project

---
+++ Source/Presentation/Wervingsreserve.Presentation.Website/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wervingsreserve.Presentation.Website
+{
+    public class Startup
+    {        
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc().AddFeatureFolders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
