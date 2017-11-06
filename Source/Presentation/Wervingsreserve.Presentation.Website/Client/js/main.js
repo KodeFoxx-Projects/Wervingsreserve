d4c88b6d49e7c98432f8aa53ec262d2f7b0d9f7b
Yves Schelpe <yves.schelpe@gmail.com>
2017-11-06 21:12:57 +0100

    added bootswatch theming

--- Source/Presentation/Wervingsreserve.Presentation.Website/Client/js/main.js
+++ Source/Presentation/Wervingsreserve.Presentation.Website/Client/js/main.js
@@ -1,4 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import '../site.css';
+//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
 import 'jquery';
 import 'popper.js/dist/popper.js';
 import 'bootstrap';
